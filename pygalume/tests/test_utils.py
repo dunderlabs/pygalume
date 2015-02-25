c8165c34fc5505af6041bc4de99943c90bf4e336
delete <pinheiro.llip@gmail.com>
2015-02-24 23:09:04 -0300

    Test para a função formating_string_name adicionado.

---
+++ pygalume/tests/test_utils.py
@@ -0,0 +1,23 @@
+import unittest
+from controller.utils import formating_string_name
+
+
+class FormatingStringTest(unittest.TestCase):
+
+	def setUp(self):
+		self.string1 = 'Pearl Jam'
+		self.string2 = 'PearlJam'
+
+	def test_was_formated(self):
+		string1_expected = 'pearl-jam'
+		result = formating_string_name(self.string1)
+		
+		self.assertEqual(string1_expected, result)
+		
+		string2_expected = 'pearljam'
+		result = formating_string_name(self.string2)
+		self.assertEqual(string2_expected, result)
+
+
+if __name__ == '__main__':
+	unittest.main()
