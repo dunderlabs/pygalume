1c5b46254c199c6ccb1584c870b810732311cf82
delete <pinheiro.llip@gmail.com>
2015-06-13 17:47:52 -0300

    Teste para quando a letra estiver expirado, adicionado.

--- pygalume/tests/test_factory.py
+++ pygalume/tests/test_factory.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from models import Lyrics
 from controller import Factory
 from myexceptions import MusicNotFound, ArtistNotFound
@@ -7,6 +9,7 @@ from tests import TestBaseDb
 class TestFactory(TestBaseDb):
 
 	def test_get_lyrics_with_db(self):
+		''' When lyrics exists in DB'''
 		lyrics = self._create()
 
 		fac = Factory(self.session)
@@ -17,14 +20,30 @@ class TestFactory(TestBaseDb):
 		self.assertEqual(str(db_lyrics), 'Testudo - Test')
 
 	def test_get_lyrics_without_db(self):
-		fac = Factory()
+		''' When lyrics does not exist in DB '''
+		fac = Factory(self.session)
 
 		db_lyrics = fac.getLyrics(artist='Pearl Jam', music='Last Kiss')
 
 		self.assertIsInstance(db_lyrics, Lyrics)
 		self.assertEqual(str(db_lyrics), 'Pearl Jam - Last Kiss')
 
-	def test_get_lyrics_without_db_error(self):
+
+	def test_get_expired_lyrics(self):
+		''' When lyrics exists and is expired '''
+		fac = Factory(self.session)
+		old_lyrics = fac.getLyrics(artist='Pearl Jam', music='Last Kiss')
+		
+		new_date = datetime.strptime('2014-04-04', '%Y-%m-%d')
+		old_lyrics.created_date = new_date
+		self.session.commit()
+
+		new_lyrics = fac.getLyrics(artist='Pearl Jam', music='Last Kiss')
+		
+		expected = datetime.now().date()
+		self.assertEqual(new_lyrics.created_date, expected)
+
+	def test_get_lyrics_raises(self):
 		fac = Factory()
 
 		with self.assertRaises(ArtistNotFound):
