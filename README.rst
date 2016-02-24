0717ecc793849cddd96f663a0a96cd7d969259bf
Patrick Mazulo <pmazulo@gmail.com>
2016-02-23 23:49:38 -0300

    Create setup.py and adapt some files

---
+++ README.rst
@@ -0,0 +1,17 @@
+.. image:: https://badge.waffle.io/indacode/pygalume.svg?label=ready&title=Ready
+   :height: 18px
+   :width: 70px
+   :alt: Stories in Ready
+   :align: left
+
+========
+pygalume
+========
+
+A simple python command line utility using the Vagalume API to search and show songs lyrics.
+
+.. image:: http://i.imgur.com/q4S2vw8.jpg
+   :height: 524px
+   :width: 582px
+   :alt: Usage
+   :align: center
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+long_description = """
+A simple python command line utility using the Vagalume API to search and show songs lyrics."""
+
+setup(
+    name='Pygalume',
+    version='0.1.0',
+    description='A simple CLI Python to get lyrics',
+    long_description=long_description,
+    url='https://github.com/dunderlabs/pygalume',
+    author='Dunderlabs Team',
+    author_email='dunderlabs@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Shells',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='cli lyrics shell',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['requests>=2.7.0', 'SQLAlchemy>=1.0.8'],
+)
+
