696ae56f3bab8a7851829df3eb52ce4609a8f558
Patrick Mazulo <pmazulo@gmail.com>
2016-02-26 00:01:56 -0300

    Update setup.py and fix bug in bin/pygalumexy

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import setup, find_packages
 
 
 long_description = """
-A simple python command line utility using the
+Pygalume is a simple python command line utility using the
 Vagalume API to search and show songs lyrics.
 """
 
 setup(
     name='Pygalume',
-    version='0.1.2',
-    description='A simple CLI Python to get lyrics',
+    version='0.1.4',
+    description='Pygalume is a simple Python CLI to get lyrics',
     long_description=long_description,
     url='https://github.com/dunderlabs/pygalume',
     author='Dunderlabs Team',
     author_email='dunderlabs@gmail.com',
+    maintainer='Dunderlabs Team',
+    maintainer_email='dunderlabs@gmail.com',
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -24,8 +26,10 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    keywords='cli lyrics shell',
+    keywords='pygalume cli lyrics shell',
+    download_url='https://pypi.python.org/pypi/Pygalume',
     packages=find_packages(exclude=['tests*']),
     install_requires=['requests>=2.7.0', 'SQLAlchemy>=1.0.8'],
     scripts=['bin/pygalume'],
+    platforms='windows linux',
 )
