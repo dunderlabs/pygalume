2d15ba4bd63999b8cc63b7fac95b3c99be98af9b
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-03-01 16:56:03 -0300

    Fix database path
    Add a default location to save the database.

--- pygalume/settings.py
+++ pygalume/settings.py
@@ -1,3 +1,14 @@
-DATABASE_URL = 'banco.db'
+import os
+from os.path import join, expanduser
+# If on Linux get, /home/$USER
+# If on Windows get, c:\\Users\\MyUser
+HOME = expanduser("~")
+
+PATH = join(HOME, '.pygalume')
+
+if not os.path.isdir(PATH):
+    os.makedirs(PATH)
+
+DATABASE_URL = join(PATH, 'banco.db')
 
 API_URL = 'http://api.vagalume.com.br/search.php?'
